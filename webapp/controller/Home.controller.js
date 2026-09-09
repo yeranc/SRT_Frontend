eabeead173910e8f27a443cf087729851bfb2f5c
chandana.yeranagu@msg-global.com <chandana.yeranagu@msg-global.com>
2026-09-09 07:57:45 +0000

    Added the tcodes to customization and customization upload

--- webapp/controller/Home.controller.js
+++ webapp/controller/Home.controller.js
@@ -1,7 +1,6 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller",
-    "sap/ushell/Container"
-], function (Controller, ) {
+    "sap/ui/core/mvc/Controller"
+], function (Controller) {
     "use strict";
 
     return Controller.extend("srt.app.controller.Home", {
@@ -9,8 +8,8 @@ sap.ui.define([
         onInit: function () {
             // Nothing to load yet - Home page is fully static.
             this.byId("heroImage").setSrc(
-        sap.ui.require.toUrl("srt/app/images/hero-background.png")
-         );
+                sap.ui.require.toUrl("srt/app/images/hero-background.png")
+            );
         },
 
         /**
@@ -18,33 +17,44 @@ sap.ui.define([
          * Each CustomListItem carries a "route" custom data value
          * that matches a route name in manifest.json.
          */
-        onCustomPress: function (oEvent) {
-            // var sRoute = oEvent.getSource().data("route");
-            // this.getOwnerComponent().getRouter().navTo(sRoute);
-            var oCrossAppNavigator =
-                sap.ushell.Container.getService("CrossApplicationNavigation");
+        onCustomPress: function () {
+            sap.ushell.Container.getServiceAsync("CrossApplicationNavigation")
+                .then(function (oCrossAppNavigator) {
+                    var sHash = oCrossAppNavigator.hrefForExternal({
+                        target: {
+                            semanticObject: "ZSRT_SO_CUSTOMIZING",
+                            action: "display"
+                        }
+                    });
 
-                sap.ushell.Conta
-            var sHash = (oCrossAppNavigator && oCrossAppNavigator.hrefForExternal({
-                target: {
-                    semanticObject: "ZSRT_SO_CUSTOMIZING",
-                    action: "display"
-                }
-            })) || "";
-            oCrossAppNavigator.toExternal({
-                target: {
-                    shellHash: sHash
-                }
-            });
-
-            // oCrossAppNavigator.toExternal({
-            //     target: {
-            //         semanticObject: "ZSRT_SO_CUSTMIZING",
-            //         action: "display"
-            //     }
-            // });
-
+                    oCrossAppNavigator.toExternal({
+                        target: { shellHash: sHash }
+                    });
+                })
+                .catch(function (oError) {
+                    sap.m.MessageToast.show("Navigation failed: " + oError.message);
+                });
         },
+
+        onCustomUpload: function (oEvent) {
+            sap.ushell.Container.getServiceAsync("CrossApplicationNavigation")
+                .then(function (oCrossAppNavigator) {
+                    var sHash = oCrossAppNavigator.hrefForExternal({
+                        target: {
+                            semanticObject: "ZSRT_SO_CUSTOM_UPLOAD",
+                            action: "display"
+                        }
+                    });
+
+                    oCrossAppNavigator.toExternal({
+                        target: { shellHash: sHash }
+                    });
+                })
+                .catch(function (oError) {
+                    sap.m.MessageToast.show("Navigation failed: " + oError.message);
+                });
+        },
+
         onCardPress: function (oEvent) {
             var sRoute = oEvent.getSource().data("route");
             this.getOwnerComponent().getRouter().navTo(sRoute);
